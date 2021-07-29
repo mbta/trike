e1415d3151f981d2dd318f20ceae511d3884c278
Beatrix Klebe <bklebe@mbta.com>
2021-07-29 15:28:07 -0400

    feat: parse messages into CloudEvents

---
+++ lib/trike/cloud_event.ex
@@ -0,0 +1,64 @@
+defmodule Trike.CloudEvent do
+  @type t() :: %__MODULE__{
+          specversion: String.t(),
+          type: String.t(),
+          source: URI.t(),
+          id: String.t(),
+          partitionkey: String.t(),
+          time: DateTime.t(),
+          data: %{
+            received_time: DateTime.t(),
+            raw: binary()
+          }
+        }
+
+  defstruct [
+    :source,
+    :id,
+    :partitionkey,
+    :time,
+    specversion: "1.0",
+    type: "com.mbta.ocs.raw_message",
+    data: %{received_time: nil, raw: nil}
+  ]
+
+  @spec parse(binary(), DateTime.t(), String.t()) :: t()
+  def parse(message, current_time, partition_key) do
+    time = message_time(message, current_time)
+    id = :crypto.hash(:blake2b, [DateTime.to_iso8601(time), message])
+
+    %__MODULE__{
+      source: event_source(),
+      time: time,
+      id: id,
+      partitionkey: partition_key,
+      data: %{
+        received_time: current_time,
+        raw: message
+      }
+    }
+  end
+
+  @spec message_time(binary(), DateTime.t()) :: DateTime.t()
+  defp message_time(message, current_time) do
+    [_count, _type, time, _rest] = String.split(message, ",", parts: 4)
+    [hour, minute, second] = String.split(time, ":")
+
+    eastern_time = DateTime.shift_zone!(current_time, "America/New_York")
+
+    %{
+      eastern_time
+      | hour: String.to_integer(hour),
+        minute: String.to_integer(minute),
+        second: String.to_integer(second),
+        microsecond: {0, 0}
+    }
+  end
+
+  @spec event_source() :: URI.t()
+  defp event_source() do
+    {:ok, app} = :application.get_application(__MODULE__)
+
+    URI.merge("ocs://opstech3.mbta.com", to_string(app))
+  end
+end
--- lib/trike/proxy.ex
+++ lib/trike/proxy.ex
@@ -1,16 +1,21 @@
 defmodule Trike.Proxy do
   use GenServer
   require Logger
+  alias Trike.CloudEvent
   alias Trike.Util
 
   @behaviour :ranch_protocol
 
   @type t() :: %__MODULE__{
           socket: :ranch_transport.socket(),
-          stream: String.t()
+          stream: String.t(),
+          partition_key: String.t(),
+          buffer: binary()
         }
 
-  defstruct socket: nil, stream: nil
+  defstruct [:socket, :stream, :partition_key, buffer: ""]
+
+  @eot <<4>>
 
   @impl :ranch_protocol
   def start_link(ref, transport, opts) do
@@ -26,29 +31,39 @@ defmodule Trike.Proxy do
   def handle_continue({ref, transport, stream}, state) do
     {:ok, socket} = :ranch.handshake(ref)
     :ok = transport.setopts(socket, active: true)
+    socket_formatted = Util.format_socket(socket)
+    partition_key = :crypto.hash(:blake2b, socket_formatted)
 
-    Logger.info("Accepted socket: #{Util.format_socket(socket)}")
+    Logger.info("Accepted socket: #{socket_formatted}")
 
-    {:noreply, %{state | socket: socket, stream: stream}}
+    {:noreply, %{state | socket: socket, stream: stream, partition_key: partition_key}}
   end
 
   @impl GenServer
   def handle_info(
         {:tcp, socket, data},
-        %{socket: socket, stream: stream} = state
+        %{socket: socket, buffer: buffer, partition_key: partition_key} = state
       ) do
-    {:noreply, state}
+    {messages, rest} = extract(buffer <> data)
+    current_time = DateTime.utc_now()
+    _events = Enum.map(messages, &CloudEvent.parse(&1, current_time, partition_key))
+
+    {:noreply, %{state | buffer: rest}}
   end
 
-  def handle_info(
-        {:tcp_closed, _socket},
-        %{stream: stream} = state
-      ) do
+  def handle_info({:tcp_closed, _socket}, state) do
     {:stop, :normal, state}
   end
 
   def handle_info(msg, state) do
     Logger.info("#{__MODULE__} unknown message: #{inspect(msg)}")
     {:noreply, state}
+  end
+
+  @spec extract(binary()) :: {[binary()], binary()}
+  defp extract(buffer) do
+    statements = String.split(buffer, @eot)
+    {messages, [rest]} = Enum.split(statements, -1)
+    {messages, rest}
   end
 end
