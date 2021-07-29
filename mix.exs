cfd1caf733dabe6d725aabd7c7f8a80cd136a5fc
Beatrix Klebe <bklebe@mbta.com>
2021-07-29 15:25:00 -0400

    chore: use Elixir 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Trike.MixProject do
     [
       app: :trike,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
