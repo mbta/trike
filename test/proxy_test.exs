0367170134359d9af5a3eab7d459044e092a5b2a
Beatrix Klebe <bklebe@mbta.com>
2021-09-09 17:46:29 -0400

    fixup! tests: add tests for events across packets

--- test/proxy_test.exs
+++ test/proxy_test.exs
@@ -34,18 +34,18 @@ defmodule ProxyTest do
         :timer.sleep(200)
       end)
 
-      assert log =~
-      """
-      console
-      {127.0.0.1:8001 -> 127.0.0.1:#{port}}
-      {"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W","received_time":"2021-08-13T12:00:00Z"},"id":"MKHkLK0QpFSsMkz8MeIw1UvgnAY2iGPTCLY+U1y4M8UVHNFTz+21I6TRrllJBAmyK3Jn3K8kwVxL3owWUFJX2g==","partitionkey":"{127.0.0.1:8001 -> 127.0.0.1:#{port}}","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T02:00:06-04:00","type":"com.mbta.ocs.raw_message"}
-      """
+    assert log =~
+             """
+             console
+             {127.0.0.1:8001 -> 127.0.0.1:#{port}}
+             {"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W","received_time":"2021-08-13T12:00:00Z"},"id":"MKHkLK0QpFSsMkz8MeIw1UvgnAY2iGPTCLY+U1y4M8UVHNFTz+21I6TRrllJBAmyK3Jn3K8kwVxL3owWUFJX2g==","partitionkey":"{127.0.0.1:8001 -> 127.0.0.1:#{port}}","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T02:00:06-04:00","type":"com.mbta.ocs.raw_message"}
+             """
 
-      assert log =~
-      """
-      console
-      {127.0.0.1:8001 -> 127.0.0.1:#{port}}
-      {"data":{"raw":"4995,TSCH,02:00:06,R,RLD,W","received_time":"2021-08-13T12:00:00Z"},"id":"USj9TixGA6R42TCmu0raEptXPYwEyXTbybn3bOaFhwKnsnMqXK2alX+NQj5v4JZ6I1UE7xwsY1qd/fv/wgwGpg==","partitionkey":"{127.0.0.1:8001 -> 127.0.0.1:#{port}}","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T02:00:06-04:00","type":"com.mbta.ocs.raw_message"}
-      """
+    assert log =~
+             """
+             console
+             {127.0.0.1:8001 -> 127.0.0.1:#{port}}
+             {"data":{"raw":"4995,TSCH,02:00:06,R,RLD,W","received_time":"2021-08-13T12:00:00Z"},"id":"USj9TixGA6R42TCmu0raEptXPYwEyXTbybn3bOaFhwKnsnMqXK2alX+NQj5v4JZ6I1UE7xwsY1qd/fv/wgwGpg==","partitionkey":"{127.0.0.1:8001 -> 127.0.0.1:#{port}}","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T02:00:06-04:00","type":"com.mbta.ocs.raw_message"}
+             """
   end
 end
