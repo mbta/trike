a914ace197c0cef9a809bea69c7193542330fcc4
Paul Swartz <paul@paulswartz.net>
2022-08-16 09:32:31 -0400

    feat: only use Splunk logger if token is configured
    Otherwise, we'll use the console logger and Docker's built-in Splunk
    support.

--- config/prod.exs
+++ config/prod.exs
@@ -3,13 +3,10 @@ import Config
 config :trike,
   kinesis_client: Trike.KinesisClient
 
-config :logger, backends: [Logger.Backend.Splunk, :console]
+config :logger, backends: [:console]
 
-config :logger, Logger.Backend.Splunk,
-  host: "https://http-inputs-mbta.splunkcloud.com/services/collector/event",
+config :logger, :console,
   format: "$dateT$time $metadata[$level] node=$node $message\n",
   level: :info
-
-config :logger, :console, level: :warn
 
 config :ehmon, :report_mf, {:ehmon, :info_report}
