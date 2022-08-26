fd016c01f8e67d4debc62080ff3965bcc6b7fdec
Paul Swartz <paul@paulswartz.net>
2022-08-26 19:37:28 -0400

    fix: use the fake KinesisClient if sending to the console

--- lib/trike/application.ex
+++ lib/trike/application.ex
@@ -9,9 +9,18 @@ defmodule Trike.Application do
   @impl true
   def start(_type, _args) do
     listen_port = Application.get_env(:trike, :listen_port)
+    kinesis_client = Application.get_env(:trike, :kinesis_client)
     kinesis_stream = Application.get_env(:trike, :kinesis_stream)
 
-    Logger.info(["Starting Trike on port ", inspect(listen_port), " proxying to ", kinesis_stream])
+    Logger.info([
+      "Starting Trike on port ",
+      inspect(listen_port),
+      " proxying to ",
+      kinesis_stream,
+      " (",
+      inspect(kinesis_client),
+      ")"
+    ])
 
     :ranch.start_listener(
       make_ref(),
@@ -19,7 +28,7 @@ defmodule Trike.Application do
       [{:port, listen_port}],
       Trike.Proxy,
       stream: kinesis_stream,
-      kinesis_client: Application.get_env(:trike, :kinesis_client),
+      kinesis_client: kinesis_client,
       clock: Application.get_env(:trike, :clock)
     )
   end
