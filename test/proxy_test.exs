854180c27aa898473301389ced06905d62fdbbc0
Paul Swartz <paul@paulswartz.net>
2022-11-07 10:52:16 -0500

    refactor: pull State construction into `setup`

--- test/proxy_test.exs
+++ test/proxy_test.exs
@@ -6,24 +6,28 @@ defmodule ProxyTest do
 
   @eot <<4>>
 
-  test "sends a properly formatted event to Kinesis" do
-    state = %{
+  setup do
+    state = %Proxy{
       buffer: "",
       partition_key: "test_key",
       clock: FakeDateTime,
       stream: "test_stream",
       put_record_fn: fn stream, key, data ->
         send(self(), {:put_record, stream, key, data})
-        {:ok, :ok}
+        {:ok, %{"SequenceNumber" => "0"}}
       end,
       socket: :socket,
       transport: __MODULE__.FakeTransport,
       received: 0
     }
 
+    {:ok, %{state: state}}
+  end
+
+  test "sends a properly formatted event to Kinesis", %{state: state} do
     data = "4994,TSCH,02:00:06,R,RLD,W#{@eot}"
 
-    Proxy.handle_info({:tcp, :socket, data}, state)
+    Proxy.handle_info({:tcp, state.socket, data}, state)
 
     event =
       ~s([{"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W"},"id":"myH7tTFo1tuZdSXxQ/5QFA4Xx58=","partitionkey":"test_key","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T12:00:00Z","type":"com.mbta.ocs.raw_message"}])
@@ -32,21 +36,7 @@ defmodule ProxyTest do
     assert_received({:setopts, :socket, active: :once})
   end
 
-  test "combines multiple OCS records into a single Kinesis batched record" do
-    state = %{
-      buffer: "",
-      partition_key: "test_key",
-      clock: FakeDateTime,
-      stream: "test_stream",
-      put_record_fn: fn stream, key, data ->
-        send(self(), {:put_record, stream, key, data})
-        {:ok, :ok}
-      end,
-      socket: :socket,
-      transport: __MODULE__.FakeTransport,
-      received: 0
-    }
-
+  test "combines multiple OCS records into a single Kinesis batched record", %{state: state} do
     data = "4994,TSCH,02:00:06,R,RLD,W#{@eot}4995,TSCH,03:00:06,R,RLD,W#{@eot}"
 
     Proxy.handle_info({:tcp, :socket, data}, state)
@@ -58,21 +48,8 @@ defmodule ProxyTest do
     assert_received({:setopts, :socket, active: :once})
   end
 
-  test "builds events with buffer" do
-    state = %{
-      buffer: "4994,TSCH,02:00:06",
-      partition_key: "test_key",
-      clock: FakeDateTime,
-      stream: "test_stream",
-      put_record_fn: fn stream, key, data ->
-        send(self(), {:put_record, stream, key, data})
-        {:ok, :ok}
-      end,
-      socket: :socket,
-      transport: __MODULE__.FakeTransport,
-      received: 0
-    }
-
+  test "builds events with buffer", %{state: state} do
+    state = %{state | buffer: "4994,TSCH,02:00:06"}
     data = ",R,RLD,W#{@eot}4995,TSCH,02:00:07"
     rest = "4995,TSCH,02:00:07"
 
@@ -96,15 +73,16 @@ defmodule ProxyTest do
     assert log =~ "Started health checker"
   end
 
-  test "logs connection string on shutdown" do
+  test "logs connection string on shutdown", %{state: state} do
     connection_string = "1.2.3.4:5 -> 6.7.8.9:10"
 
     shutdown_log =
       capture_log(fn ->
-        Proxy.handle_info({:tcp_closed, "socket"}, %{partition_key: connection_string})
+        Proxy.handle_info({:tcp_closed, state.socket}, %{state | partition_key: connection_string})
       end)
 
-    assert shutdown_log =~ "Socket closed: #{inspect(connection_string)}"
+    assert shutdown_log =~ "Socket closed"
+    assert shutdown_log =~ inspect(connection_string)
   end
 
   defmodule FakeTransport do
