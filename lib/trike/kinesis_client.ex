51d9dcade56d7dd1bab7e4d22f344f550e677aa7
Paul Swartz <paul@paulswartz.net>
2022-11-07 11:04:14 -0500

    feat: pass SequenceNumberForOrdering to ensure order
    Documentation:
    https://docs.aws.amazon.com/kinesis/latest/APIReference/API_PutRecord.html
    
    SequenceNumberForOrdering (using the SequenceNumber from the previous
    record) allows Kinesis to enforce the message order. Otherwise, it's
    based on arrival time.

--- lib/trike/kinesis_client.ex
+++ lib/trike/kinesis_client.ex
@@ -8,8 +8,9 @@ defmodule Trike.KinesisClient do
   Puts a new record in the provided AWS Kinesis Data Stream with the given
   partition key.
   """
-  @spec put_record(Kinesis.stream_name(), binary(), binary()) :: {:ok, term()} | {:error, term()}
-  def put_record(stream, partition_key, event) do
-    ExAws.request(Kinesis.put_record(stream, partition_key, event))
+  @spec put_record(Kinesis.stream_name(), binary(), binary(), Keyword.t()) ::
+          {:ok, %{String.t() => String.t()}} | {:error, term()}
+  def put_record(stream, partition_key, event, opts \\ []) do
+    ExAws.request(Kinesis.put_record(stream, partition_key, event, opts))
   end
 end
