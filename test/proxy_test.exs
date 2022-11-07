721d3143b57b5374a8bf96e6a5a0a14fa32350aa
Paul Swartz <paul@paulswartz.net>
2022-11-07 10:36:22 -0500

    feat: batch OCS messages from a single read into a single write
    This increases the speed at which records can get into Kinesis while
    maintaining the order of messages.

--- test/proxy_test.exs
+++ test/proxy_test.exs
@@ -26,7 +26,33 @@ defmodule ProxyTest do
     Proxy.handle_info({:tcp, :socket, data}, state)
 
     event =
-      ~s({"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W"},"id":"myH7tTFo1tuZdSXxQ/5QFA4Xx58=","partitionkey":"test_key","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T12:00:00Z","type":"com.mbta.ocs.raw_message"})
+      ~s([{"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W"},"id":"myH7tTFo1tuZdSXxQ/5QFA4Xx58=","partitionkey":"test_key","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T12:00:00Z","type":"com.mbta.ocs.raw_message"}])
+
+    assert_received({:put_record, "test_stream", "test_key", ^event})
+    assert_received({:setopts, :socket, active: :once})
+  end
+
+  test "combines multiple OCS records into a single Kinesis batched record" do
+    state = %{
+      buffer: "",
+      partition_key: "test_key",
+      clock: FakeDateTime,
+      stream: "test_stream",
+      put_record_fn: fn stream, key, data ->
+        send(self(), {:put_record, stream, key, data})
+        {:ok, :ok}
+      end,
+      socket: :socket,
+      transport: __MODULE__.FakeTransport,
+      received: 0
+    }
+
+    data = "4994,TSCH,02:00:06,R,RLD,W#{@eot}4995,TSCH,03:00:06,R,RLD,W#{@eot}"
+
+    Proxy.handle_info({:tcp, :socket, data}, state)
+
+    event =
+      ~s([{"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W"},"id":"myH7tTFo1tuZdSXxQ/5QFA4Xx58=","partitionkey":"test_key","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T12:00:00Z","type":"com.mbta.ocs.raw_message"},{\"data\":{\"raw\":\"4995,TSCH,03:00:06,R,RLD,W\"},\"id\":\"O7ODUPlPMM089UZL1YLYpFIZzeo=\",\"partitionkey\":\"test_key\",\"source\":\"opstech3.mbta.com/trike\",\"specversion\":\"1.0\",\"time\":\"2021-08-13T12:00:00Z\",\"type\":\"com.mbta.ocs.raw_message\"}])
 
     assert_received({:put_record, "test_stream", "test_key", ^event})
     assert_received({:setopts, :socket, active: :once})
@@ -53,7 +79,7 @@ defmodule ProxyTest do
     {:noreply, %{buffer: buffer}} = Proxy.handle_info({:tcp, :socket, data}, state)
 
     event =
-      ~s({"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W"},"id":"myH7tTFo1tuZdSXxQ/5QFA4Xx58=","partitionkey":"test_key","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T12:00:00Z","type":"com.mbta.ocs.raw_message"})
+      ~s([{"data":{"raw":"4994,TSCH,02:00:06,R,RLD,W"},"id":"myH7tTFo1tuZdSXxQ/5QFA4Xx58=","partitionkey":"test_key","source":"opstech3.mbta.com/trike","specversion":"1.0","time":"2021-08-13T12:00:00Z","type":"com.mbta.ocs.raw_message"}])
 
     assert_received({:put_record, "test_stream", "test_key", ^event})
 
