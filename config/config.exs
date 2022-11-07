6a166a4eb603924ae1d8fcce23e79213d4bb9f90
Paul Swartz <paul@paulswartz.net>
2022-11-07 09:49:59 -0500

    feat: swith from Hackney to Req for ExAws requests

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,14 @@ import Config
 
 config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 
-config :ex_aws, json_codec: Jason
+config :ex_aws,
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, {:awscli, "default", 30}],
+  secret_access_key: [
+    {:system, "AWS_SECRET_ACCESS_KEY"},
+    {:awscli, "default", 30}
+  ],
+  http_client: ExAws.Request.Req,
+  json_codec: Jason
 
 config :trike,
   kinesis_client: Fakes.FakeKinesisClient,
