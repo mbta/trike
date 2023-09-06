cb34487fc8ce0dbab336ca89ae40878f2b4cd368
Nick Stein <nstein@mbta.com>
2023-09-06 11:41:30 -0400

    feat: add sourceip as a direct field on each event (#34)
    * feat: add sourceip as a direct field on each event
    
    * wip: apply formatting

--- lib/trike/cloud_event.ex
+++ lib/trike/cloud_event.ex
@@ -12,26 +12,28 @@ defmodule Trike.CloudEvent do
           id: String.t(),
           partitionkey: String.t(),
           time: DateTime.t(),
-          data: OcsRawMessage.t()
+          data: OcsRawMessage.t(),
+          sourceip: String.t()
         }
 
   @derive Jason.Encoder
-  @enforce_keys [:id, :partitionkey, :time, :data]
+  @enforce_keys [:id, :partitionkey, :sourceip, :time, :data]
   defstruct @enforce_keys ++
               [
-                source: "opstech3.mbta.com/trike",
+                source: "#{:inet.gethostname() |> elem(1)}.mbta.com/trike",
                 specversion: "1.0",
                 type: "com.mbta.ocs.raw_message"
               ]
 
   @doc """
-  Creates a CloudEvent struct given a full OCS message, the current time, and a partition key.
+  Creates a CloudEvent struct given a full OCS message, the current time, a partition key, and the source IP.
   """
-  @spec from_ocs_message(binary(), DateTime.t(), String.t()) :: t()
-  def from_ocs_message(message, current_time, partition_key) do
+  @spec from_ocs_message(binary(), DateTime.t(), String.t(), String.t()) :: t()
+  def from_ocs_message(message, current_time, partition_key, source_ip) do
     %__MODULE__{
       id: :crypto.hash(:sha, [DateTime.to_iso8601(current_time), message]) |> Base.encode64(),
       partitionkey: partition_key,
+      sourceip: source_ip,
       time: current_time,
       data: %OcsRawMessage{raw: message}
     }
